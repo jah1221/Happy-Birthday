6d5c3902d4312b9093de6f5cd146e9da50e3d00e
jah1221 <152050838+jah1221@users.noreply.github.com>
2023-11-26 16:02:38 +0800

    Add files via upload

---
+++ script-1.js
@@ -0,0 +1,6 @@
+document.getElementById("birthdayButton").addEventListener("click",function() {
+    const message = document.getElementById("birthdayMessage");
+    message.innerHTML = "Happiest Birthday Eunice!! Thank you for everything my honeybunch, thank you for listening to my nonsense kaartehans. I will always be thankful for your existence. Palagi akong nandito for you, I love you so much uning! miss na kita bwisitin😝 Happy na Birthday mo pa, HA PI BERT DEY!🎂💞";
+    message.style.animation = "fadeIn 2s forwards";
+   
+} );
